157193463549330fb40c93713cc12ca19d71a0a9
CHEN Longzan <15950502686@163.com>
2019-08-26 12:32:00 +0800

    2019,8,26

--- threeAndWebgl/learn_three_js/05.js
+++ threeAndWebgl/learn_three_js/05.js
@@ -6,9 +6,12 @@ let scene = new THREE.Scene();
 
 
 //圆弧--点渲染模式
-let shape = new THREE.Shape();
-shape.absarc(0,0,100,0,0.5*Math.PI);
-let geometry = new THREE.ShapeGeometry(shape);
+// let shape = new THREE.Shape();
+// shape.absarc(0,0,100,0,0.5*Math.PI);
+// let geometry = new THREE.ShapeGeometry(shape);
+//
+//
+//
 // let material = new THREE.PointsMaterial({
 //     color:0x0000ff,
 //     size:10.0
@@ -23,14 +26,118 @@ let geometry = new THREE.ShapeGeometry(shape);
 // let line = new THREE.Line(geometry,material);
 
 //面渲染模式
-let material = new THREE.MeshBasicMaterial({
-    color:0x0000ff
+// let material = new THREE.MeshBasicMaterial({
+//     color:0x0000ff
+// });
+// let line = new THREE.Mesh(geometry,material);
+
+//
+// scene.add(line);
+
+
+
+
+
+//创建一个拉伸网格模型
+// let shape = new THREE.Shape();
+// shape.moveTo(0,0);
+// shape.lineTo(0,100);
+// shape.lineTo(100,100);
+// shape.lineTo(100,0);
+// shape.lineTo(0,0);
+//
+// let geometry = new THREE.ExtrudeGeometry(//拉伸造型
+//     shape,//二维轮廓
+//     {
+//         depth:120,//拉伸长度
+//         bevelEnabled:false//无倒角
+//     }
+// );
+//
+// let material = new THREE.MeshBasicMaterial({color:0x0000ff});
+// let mesh = new THREE.Mesh(geometry,material);
+// scene.add(mesh);
+
+//创建扫描网格模型
+let shape = new THREE.Shape();
+//创建四条直线绘制矩形
+shape.moveTo(0,0);
+shape.lineTo(0,10);
+shape.lineTo(10,10);
+shape.lineTo(10,0);
+shape.lineTo(0,0);
+//创建罗阔的扫描轨迹（3D样条曲线）
+let curve = new THREE.SplineCurve3([
+    new THREE.Vector3(-10,-50,-50),
+    new THREE.Vector3(10,0,0),
+    new THREE.Vector3(8,50,50),
+    new THREE.Vector3(-5,0,100)
+]);
+
+
+let geometry = new THREE.ExtrudeGeometry(
+    shape,
+    {
+        bevelEnabled:false,
+        extrudePath:curve,
+        steps:50
+    }
+);
+
+let material = new THREE.MeshPhongMaterial({color:0x0000ff});
+let mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
+
+
+
+
+
+
+
+
+
+let srcPoint = new THREE.Geometry();
+let pointPosition = new THREE.Vector3(0,0,0);
+srcPoint.vertices.push(pointPosition);
+let pointMaterial = new THREE.PointsMaterial({
+    color:0xdc3023,
+    size:20.0
 });
-let line = new THREE.Mesh(geometry,material);
+let newPoint = new THREE.Points(srcPoint,pointMaterial);
+scene.add(newPoint);
 
 
-scene.add(line);
+let srcLine = new THREE.Geometry();
+srcLine.vertices.push(
+    new THREE.Vector3(0,0,0),
+    new THREE.Vector3(1000,0,0),
+    new THREE.Vector3(0,0,0),
+    new THREE.Vector3(0,1000,0),
+    new THREE.Vector3(0,0,0),
+    new THREE.Vector3(0,0,1000)
+);
+let lineMaterial = new THREE.LineBasicMaterial({
+    color:0xFF6F00
+    // color:0xdc3023
+});
+let newLine = new THREE.LineSegments(srcLine,lineMaterial);
+scene.add(newLine);
 
+
+
+
+
+
+
+
+
+
+
+
+
+let point = new THREE.PointLight(0xFFFFFF);
+point.position.set(0,0,500);
+scene.add(point);
 
 /**
  * 相机设置
@@ -42,7 +149,7 @@ let s = 100;
 
 let camera = new THREE.OrthographicCamera(-s*k,s*k,s,-s,1,1000);
 // camera.position.set(0,0,500);
-camera.position.set(0,0,500);
+camera.position.set(300,300,300);
 camera.lookAt(scene.position);//设置相机的方向指向场景对象
 
 /**
@@ -60,3 +167,6 @@ function render(){
     requestAnimationFrame(render);
 }
 render();
+
+let controls = new THREE.OrbitControls(camera);//创建控件对象
+controls.addEventListener('change', render);//监听鼠标、键盘事件
